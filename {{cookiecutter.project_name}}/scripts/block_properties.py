292f92ce7ebe0a1e63cc2ecf5817ae26ed1af686
camplowell <camplowell@gmail.com>
2024-12-05 15:32:23 -0500

    Update template to latest

--- {{cookiecutter.project_name}}/scripts/block_properties.py
+++ {{cookiecutter.project_name}}/scripts/block_properties.py
@@ -1,28 +1,38 @@
-from block_wrangler import blocks, load_tags, BlockMapping
+from block_wrangler import *
 from pathlib import Path
 
-shaderpack_root = Path(__file__).parent.parent
+from caseconverter import pascalcase
+
+shaderpack_root = Path(__file__).parent
 
 def main():
     tags = load_tags()
-
+    
+    Bool = Flag.Config(function_name=lambda flag: f"Is{pascalcase(flag)}")
+    Sequence = FlagSequence.Config(function_name=lambda flag: f"Get{pascalcase(flag)}")
+    Enum = EnumFlag.Config(function_name=lambda flag: f"{pascalcase(flag)}Type") | Sequence
+    Int = IntFlag.Config() | Sequence
+    
     mapping = BlockMapping.solve({
-        'sway': tags['sway'],
-        'sway_lower': tags['sway/lower'] + tags['sway/short'],
-        'sway_upper': tags['sway/upper'],
-        'sway_hanging': tags['sway/hanging'],
-        'sway_full': tags['sway/whole'],
-        'sway_floating': tags['sway/floating'],
-        'flowers': tags['plant/flowers'],
-        'crops': tags['plant/crops'],
-        'water': blocks('minecraft:water'),
-    })
-
-
+        'sway':Enum({
+            'top': tags['sway/upper'],
+            'bottom': tags['sway/lower'] + tags['sway/short'], # Tags can be combined with the +, -, and & operators
+            'full': tags['sway/full'],
+            'floating': tags['sway/floating'],
+            'hanging': tags['sway/hanging'],
+        }),
+        'crops': Bool(tags['minecraft:crops']), # Vanilla tags are included
+        'water': Bool(blocks('minecraft:water')), # Individual blocks can also be referenced by name
+        'emissivity': Int({i:tags[f'lights/{i}'] for i in range(1, 16)}) # Flag sequences that return ints and floats are specified with IntFlag and FloatFlag, respectively.
+    }, config=MappingConfig(
+        start_index=10
+    ))
     with shaderpack_root.joinpath('shaders/block.properties').open('w') as f:
         f.write(mapping.render_encoder())
     with shaderpack_root.joinpath('shaders/util/block_properties.glsl').open('w') as f:
         f.write(mapping.render_decoder())
+    
+    print(f'Done!')
 
 if __name__ == '__main__':
     main()
